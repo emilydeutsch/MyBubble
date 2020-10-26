288e9da7c105691484f7054d14c033e2d2f1a14c
Charles Zhang <charleszha2000@gmail.com>
2020-10-25 19:41:25 -0600

    notifications

--- backend/notifications/server.js
+++ backend/notifications/server.js
@@ -9,8 +9,15 @@ const notification_options = {
 };
 
 router.post('/test', (req, res)=>{
-    const  registrationToken = req.body.registrationToken
-    const message = req.body.message
+    const  registrationToken = "AAAAvVhhZSM:APA91bEE1X5U7KJYi7-uz_4Df2RdkxkJeOKxXPmAMrClQSCF4_RLJkWhAJVOunKUzsUySpT6KgkUFRCG6n7uoIP7hz_Wiso56qe2nXS2jazgUbuoJGYSeqazI-lYw8WbXiScui_5HT6B"
+    const message = {
+      data : {},
+      notification : {
+        body: "this is a notification",
+        title: "it sure is",
+      },
+    }
+    
     const options =  notification_options
     
       admin.messaging().sendToDevice(registrationToken, message, options)
@@ -26,3 +33,4 @@ router.post('/test', (req, res)=>{
 })
 
 module.exports = router;
+
