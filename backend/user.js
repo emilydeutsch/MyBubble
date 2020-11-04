8a62a07ac2217f2f7e8212ac0d6aa7c21dee43df
Charles Zhang <charleszha2000@gmail.com>
2020-11-04 03:25:38 -0700

    basic implementation of temporary connections

--- backend/user.js
+++ backend/user.js
@@ -53,18 +53,19 @@ router.post('/addFirstConnection', async (req, res) => {
     let firstUser, secondUser;
 
     try {
-        firstUser = await userModel.find({_id: req.body.firstID});
-        secondUser = await userModel.find({_id: req.body.secondID});
+        firstUser = (await userModel.find({_id: req.body.firstID}))[0];
+        secondUser = (await userModel.find({_id: req.body.secondID}))[0];
 
-        if(!firstUser[0].firstConnections.includes(secondUser[0]._id)){
-            firstUser[0].firstConnections.push(secondUser[0]._id.toString());
-            secondUser[0].firstConnections.push(firstUser[0]._id.toString());
+        if(!firstUser.firstConnections.includes(secondUser._id)){
+            firstUser.firstConnections.push(secondUser._id.toString());
+            secondUser.firstConnections.push(firstUser._id.toString());
         }
         
-        await firstUser[0].save();
-        await secondUser[0].save();
+        await firstUser.save();
+        await secondUser.save();
 
-        res.json(firstUser.concat(secondUser));
+        let retArray = [firstUser, secondUser];
+        res.json(retArray);
     }
     catch(err){
         res.writeHead(412);
@@ -87,6 +88,70 @@ router.get('/getAllConnections', async (req, res) => {
         let user = (await userModel.find({_id: userID}))[0];
         let connections = await networkManager.findAllConnections(user);
         res.json(connections)
+
+    } catch(err) {
+        res.writeHead(412);
+        res.write(err.toString());
+        res.send();
+    }
+})
+
+router.post('/addTemporaryConnection', async (req, res) =>{
+    if(!req.body.firstID || !req.body.secondID || !req.body.date){
+        res.writeHead(412, {'Content-Type' : 'text-plain'});
+        res.write('Failed: Missing Fields');
+        res.send();
+        return;
+    }
+
+    try {
+        let firstUser = (await userModel.find({_id: req.body.firstID}))[0];
+        let secondUser = (await userModel.find({_id: req.body.secondID}))[0];
+
+        if(firstUser.firstConnections.includes(req.body.secondID)){
+            let err = new Error('Already a first level connection');
+            throw err;
+        }
+
+        for(let i = 0; i < firstUser.temporaryConnections.length; i++){
+            if(firstUser.temporaryConnections[i]._id.toString() == secondUser._id.toString()){
+                let err = new Error('Already a temporary connection');
+                throw err;
+            }
+        }
+
+        let tempConnectionForFirst = {_id: secondUser._id.toString(), date: req.body.date}
+        let tempConnectionForSecond = {_id: firstUser._id.toString(), date: req.body.date}
+
+        firstUser.temporaryConnections.push(tempConnectionForFirst)
+        secondUser.temporaryConnections.push(tempConnectionForSecond)
+
+        await firstUser.save();
+        await secondUser.save();
+
+        let retArray = [firstUser, secondUser];
+        res.json(retArray);
+    } catch(err){
+        res.writeHead(412);
+        res.write(err.toString());
+        res.send();
+    }
+})
+
+router.get('/getTemporaryConnections', async (req, res) => {
+    if(!req.query._id){
+        res.writeHead(412, {'Content-Type' : 'text-plain'});
+        res.write('Failed: Missing User IDs or invalid');
+        res.send();
+        return;
+    }
+
+    let userID = req.query._id;
+
+    try {
+        let user = (await userModel.find({_id: userID}))[0];
+        let temporaryConnections = user.temporaryConnections;
+        res.json(temporaryConnections)
 
     } catch(err) {
         res.writeHead(412);
